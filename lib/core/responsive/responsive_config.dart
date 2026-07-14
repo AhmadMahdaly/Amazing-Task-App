3f45267ab302a49722746bf72af2ea04ab224b4e
Ahmed Mahmoud <ahmed.mahdaly86@gmail.com>
2026-07-14 17:13:41 +0300

    chore: update version to 1.0.32+32 and modify responsive configuration for improved device handling

--- lib/core/responsive/responsive_config.dart
+++ lib/core/responsive/responsive_config.dart
@@ -5,73 +5,68 @@ final Size deviceSize = Size(
   SizeConfig.isTablet ? 1668 : 402,
   SizeConfig.isTablet ? 2388 : 874,
 );
-// const Size deviceSize = Size(390, 844);
-
-enum DeviceType { phone, tablet, desktop }
 
 abstract class SizeConfig {
   static late MediaQueryData _mediaQueryData;
   static late double screenWidth;
   static late double screenHeight;
-  static late DeviceType deviceType;
+  static late double _safeAreaHorizontal;
+  static late double _safeBlockHorizontal;
+  static late bool isTablet;
+
   static T responsiveValue<T>({required T phone, required T tablet}) {
     return isTablet ? tablet : phone;
   }
 
   static void init(BuildContext context) {
     _mediaQueryData = MediaQuery.of(context);
+
     screenWidth = _mediaQueryData.size.width;
+    isTablet = screenWidth >= 600;
     screenHeight = _mediaQueryData.size.height;
 
-    if (screenWidth >= 1100) {
-      deviceType = DeviceType.desktop;
-    } else if (screenWidth >= 600) {
-      deviceType = DeviceType.tablet;
-    } else {
-      deviceType = DeviceType.phone;
-    }
+    _safeAreaHorizontal =
+        _mediaQueryData.padding.left + _mediaQueryData.padding.right;
+
+    _safeBlockHorizontal = screenWidth - _safeAreaHorizontal;
   }
 
-  static bool get isDesktop => deviceType == DeviceType.desktop;
-  static bool get isTablet => deviceType == DeviceType.tablet;
-  static bool get isPhone => deviceType == DeviceType.phone;
-
-  /// عرض العناصر
-  static double w(double value) {
-    if (isDesktop) return value; // ثابت
-    return screenWidth * (value / deviceSize.width);
+  static double getWidth(double width) {
+    return SizeConfig.isTablet
+        ? screenWidth * ((width * 3) / deviceSize.width)
+        : screenWidth * (width / deviceSize.width);
   }
 
-  /// ارتفاع العناصر
-  static double h(double value) {
-    if (isDesktop) return value;
-    return screenHeight * (value / deviceSize.height);
+  static double getHeight(double height) {
+    return SizeConfig.isTablet
+        ? screenHeight * ((height * 3) / deviceSize.height)
+        : screenHeight * (height / deviceSize.height);
   }
 
-  /// الخط
-  static double sp(double size) {
-    if (isDesktop) return size; // ثابت
-    return size * (screenWidth / deviceSize.width);
+  static double getFontSize(double size) {
+    final scale = _safeBlockHorizontal / 100;
+
+    return SizeConfig.isTablet
+        ? ((size * 1.3) * scale).clamp((size * 1.3) * 0.85, (size * 1.3) * 1.15)
+        : (size * scale).clamp(size * 0.85, size * 1.15);
   }
 
-  /// نصف القطر
-  static double r(double radius) {
-    return isDesktop ? radius : w(radius);
+  static double getRadius(double r) {
+    return getWidth(r);
   }
 }
 
 extension ResponsiveSized on num {
-  double get w => SizeConfig.w(toDouble());
-  double get h => SizeConfig.h(toDouble());
-  double get sp => SizeConfig.sp(toDouble());
-  double get r => SizeConfig.r(toDouble());
-
+  double get w => SizeConfig.getWidth(toDouble());
+  double get h => SizeConfig.getHeight(toDouble());
+  double get r => SizeConfig.getRadius(toDouble());
+  double get sp => SizeConfig.getFontSize(toDouble());
   SizedBox get verticalSpace => SizedBox(height: h);
   SizedBox get horizontalSpace => SizedBox(width: w);
 }
 
-/// --->
+/// =>
 /// 100.w
 /// 50.h
 /// 16.sp
-/// verticalSpace و horizontalSpace
+/// hSpace و wSpace
